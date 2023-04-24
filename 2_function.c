1765229a4784fce2b35d12c03a62c8c1cd045221
davidkayumba <davidkayumba11@gmail.com>
2023-04-24 04:50:00 -0700

    2_function.c

--- 2_function.c
+++ 2_function.c
@@ -11,7 +11,7 @@
  * Return: Number of chars printed
  */
 int print_non_printable(va_list types, char buffer[],
-		int flags, int width, int precesion, int size)
+		int flags, int width, int precision, int size)
 {
 	char = va_arg(types, char *);
 	int count = 0;
@@ -85,51 +85,4 @@ int print_pointer(va_list types, char buffer[],
 
 	return (write_pointer(buffer, ind, length,
 				width, flags, padd, extra_c, padd_start));
-}
-
-/**
- * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
- * @ind: ind.
- * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: 1 or 2;
- */
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-	int flags, int width, int precision, int size)
-{
-	int printed_chars = -1;
-	int unknown_len = 0;
-	fmt_t fmt_types[] = {
-		{'c', print_char}, {'s', print_string},
-		{'%', print_percent}, {'i', print_int},
-		{'d', print_int}, {'b', print_binary},
-		{'u', print_unsigned}, {'o', print_octal},
-		{'x', print_hexadecimal}, {'X', print_hexa_upper},
-		{'p', print_pointer}, {'S', print_non_printable},
-		{'r', print_reverse}, {'R', print_rot13string},
-		{'\0', NULL}
-	};
-	for (i = 0; fmt_types[i].fmt != '\0'; i++)
-			if (fmt[*ind] == fmt_types[i].fmt)
-				return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
-			}
-	if (fmt[*ind] == '\0')
-	{
-		return (-1);
-		}
-		if (width)
-			--(*ind);
-			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
-				--(*ind);
-			if (fmt[*ind] == ' ')
-				--(*ind);
-			return (1);
-		}
-		unknown_len += write(1, &fmt[*ind], 1);
-		return (unknown_len);
 }
