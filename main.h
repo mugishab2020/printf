314f80f09894373a42c57931ea7e5187c68ac33c
mugishab2020 <mugishab2020@gmail.com>
2023-04-24 00:08:35 -0700

    Handle_ printing function added

--- main.h
+++ main.h
@@ -34,7 +34,7 @@ struct fmt
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i, va_list list; char buffer[],
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		int flags, int width, int precision, int size);
 
 /* Function to print chars and strings */
