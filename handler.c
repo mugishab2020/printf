6d47c3ead3db1cb321f632cdf3f33bdb5003cc6a
mugishab2020 <mugishab2020@gmail.com>
2023-04-25 16:36:49 -0700

    nothing

--- handler.c
+++ handler.c
@@ -44,7 +44,7 @@ int  percentage_handler(const char *str, va_list list, int *i)
 	format formats[] = {
 		{'s', print_string}, {'c', print_char},
 		{'d', print_integer}, {'i', print_integer},
-		{'b', print_binary}, {'o', print_unsigned},
+		{'b', print_binary}, {'u', print_unsigned},
 		{'o', print_octal}, {'x', print_hexadecimal_low},
 		{'X', print_hexadecimal_upp}, {'p', print_pointer},
 		 {'r', print_rev_string}, {'R', print_rot}
