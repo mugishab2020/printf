a6cd7944c5c12f06d546031895670c46d6d520ee
mugishab2020 <mugishab2020@gmail.com>
2023-04-25 02:04:59 -0700

    betty is fine

--- get_flags.c
+++ get_flags.c
@@ -7,22 +7,22 @@
  */
 int get_flags(const char *format, int *i)
 {
-nt a, curr;
+int a, curr;
 int flags = 0;
 const char FLAGS_CH[] = {'-', '+', '0', '#', '\0'};
 const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
 for (curr = *i + 1; format[curr] != '\0'; curr++)
 {
-	for (j = 0; FLAGS_CH[j] != '\0'; j++)
+	for (a = 0; FLAGS_CH[a] != '\0'; a++)
 	{
 		if (format[curr] == FLAGS_CH[j])
 		{
-			flags |= FLAGS_ARR[j];
+			flags |= FLAGS_ARR[a];
 			break;
 		}
 	}
-	if (FLAGS_CH[j] == 0)
+	if (FLAGS_CH[a] == 0)
 
 		break;
 
--- main.h
+++ main.h
@@ -14,7 +14,6 @@
 #define F_ZERO 4
 #define F_HASH 8
 #define	F_SPACE 16
-
 /* SIZE */
 #define S_LONG 2
 #define S_SHORT 1
